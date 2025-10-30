da8ab474de2b6801d0b89e77f35a6e28d38898b5
mateus lima <matheus2022y@gmail.com>
2025-10-30 19:43:10 -0300

    criei a pasta puser e post

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth-module';
+import { UsersModule } from './users/users.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule, UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
